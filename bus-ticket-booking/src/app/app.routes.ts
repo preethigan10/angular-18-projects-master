9c0efecedb4fb58f58a512f592ffdd69063899e3
Preethi <preethiganesh10@gmail.com>
2026-03-09 11:23:06 +0530

    pages added

--- bus-ticket-booking/src/app/app.routes.ts
+++ bus-ticket-booking/src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { BookTicketComponent } from './pages/book-ticket/book-ticket.component';
+import { MyBookingsComponent } from './pages/my-bookings/my-bookings.component';
+import { SearchComponent } from './pages/search/search.component';
+import { SearchResultComponent } from './pages/search-result/search-result.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'search',
+        pathMatch: 'full'
+    },
+    {
+        path: 'book-ticket',
+        component: BookTicketComponent
+    },
+    {
+        path: 'my-booking',
+        component: MyBookingsComponent
+    },
+    {
+        path: 'search',
+        component: SearchComponent
+    },
+    {
+        path: 'search-result',
+        component: SearchResultComponent
+    },
+
+];
---
+++ bus-ticket-booking/src/app/pages/search/search.component.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+
+@Component({
+  selector: 'app-search',
+  imports: [],
+  templateUrl: './search.component.html',
+  styleUrl: './search.component.css'
+})
+export class SearchComponent {
+
+
+
+  
+}
